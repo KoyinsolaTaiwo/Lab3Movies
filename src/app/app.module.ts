a96eb99146ee7909448af4edeaedd1cc2341124d
KOYINSOLA TAIWO - STUDENT <G00331148@gmit.ie>
2019-10-07 11:57:55 +0100

    full code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,17 +14,24 @@ MatButtonModule,
 MatToolbarModule,
 MatExpansionModule
 } from '@angular/material';
+import { ReadComponent } from './read/read.component';
+import { WriteComponent } from './write/write.component';
+import { CreateComponent } from './create/create.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReadComponent,
+    WriteComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule,
     MatCardModule,
 MatButtonModule,
 MatToolbarModule,
---
+++ src/app/read/read.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {MovieServiceService} from '../Services/movie-service.service';
+
+@Component({
+  selector: 'app-read',
+  templateUrl: './read.component.html',
+  styleUrls: ['./read.component.css']
+})
+export class ReadComponent implements OnInit {
+
+    //title = 'movie-app';
+    MyMovies: any = [];
+
+    constructor(private movieService: MovieServiceService){}
+
+  ngOnInit(){
+    this.movieService.GetMovieInformation().subscribe((data) => {
+      this.MyMovies = data.Search;
+      console.log(this.MyMovies);
+    })
+
+}
+}
